db5c7e72c36d0497b5bb23803ad9096d9fd5116f
Leonardo Camelo <112513833+LeonardoCaml@users.noreply.github.com>
2023-01-02 19:34:36 -0300

    Create areaDoCliente.js

---
+++ js/areaDoCliente.js
@@ -0,0 +1,10 @@
+const add = document.getElementById("addServices");
+const containter = document.getElementById("container");
+
+function openBar() {
+    add.addEventListener("click", () => {
+       alert("bora");
+    });
+}
+
+openBar();
