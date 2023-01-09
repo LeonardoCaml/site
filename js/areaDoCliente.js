7e392a53992cde33c8c3733e613063cdd13a1c1e
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-01-09 18:39:08 -0300

    Create areaDoCliente.js

---
+++ js/areaDoCliente.js
@@ -0,0 +1,12 @@
+const add = document.getElementById("addServices");
+const containter = document.getElementById("container");
+
+function openBar() {
+    add.addEventListener("click", () => {
+       containter.style.display = "flex";
+    });
+    
+    
+}
+
+openBar();
