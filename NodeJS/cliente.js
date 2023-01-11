b803680c94e4f2b26b37cf20286a78d02ce1947f
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-01-10 22:23:46 -0300

    nodeJS
    arquivos node para criar o banco  de dados

---
+++ NodeJS/cliente.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const database = require('./db');
+
+const cliente = database.define('cliente', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+    },
+    senha: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+    }
+})
