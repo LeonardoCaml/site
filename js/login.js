86be1a164129d60707d3a0da4b4fbd077247e657
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-01-12 17:26:28 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,61 +1,57 @@
-const clientDataBase = require('../NodeJS/src/cliente');
-
 class Cliente {
-    constructor(id, nome, senha) {
+    constructor(id, email, senha) {
         this.id = id;
-        this.nome = nome;
+        this.email = email;
         this.senha = senha;
     }
 }
 
-function emailValid() {
-    
+function validate() {
+    const form = document.querySelector('form')
+    const email = document.getElementById('email'); 
+    const senha = document.getElementById('senha');
+    const error = document.getElementById('error');
+    const errorEmail = document.getElementById('errorEmail');
+    const errorPassword = document.getElementById('error');
+
+    form.addEventListener('submit', (e) => {
+
+        if(email.value == '' && senha.value == '') {
+            error.textContent = 'insira seus dados nos campos!';
+        } else {
+            console.log(email.value);
+            console.log(senha.value);
+        }
+
+        e.preventDefault();
+    })
+
+    email.addEventListener('keyup', () => {
+        if(emailParams(email.value) !== true) {
+            errorEmail.textContent = "insira um formato válido (@gmail, @hotmail, @yahoo, ...)"
+        } else {
+            errorEmail.textContent = '';
+        }
+    })
 }
 
-function clientReturn() {
+function emailParams(email) {
+    let emailPath = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
 
-    const client = new Cliente();
+    return emailPath.test(email);
+
+}
+
+function clientLogin() {
+
+    const client = new Cliente()
+
     client.id = 1;
-    client.nome = 'João';
-    client.senha = 123;
-    
-    
-    console.log(client)
+    client.email = 'leonardocamelo20@gmail.com';
+    client.senha = 'minecraft.it.net';
 
+    return client;
 }
 
-clientReturn();
-
-
-
-
-// function variableFields() {
-//     const emailValid = isEmailValid();
-//     const passwordValid =isPasswordValid();
-
-//     if(!emailValid || !passwordValid) {
-//         entrar.disabled = true;
-//     } else {
-//         entrar.remove(disabled);
-//     }
-// }
-
-// function isEmailValid() {
-//     const email = document.getElementById("email").value;
-//     if (!email) {
-//         return false;
-//     }
-//     return validateEmail(email);
-// }
-
-// function isPasswordValid() {
-//     const password = document.getElementById("senha").valiue;
-//     if (!password) {
-//         return false;
-//     }
-//     return true;
-// }
-
-// function validateEmail(email) {
-//     return /\S+@\S+\.\S+/.test(email);
-// }
+clientLogin();
+validate();
